8314f1743fb951dd6371231dde0a978e575a6b6d
Alberto Miorin <github@ululi.it>
2015-02-26 14:18:23 +0100

    better monitor, rename _uuid to _txid, make _uuid unique

--- examples/monitor.js
+++ examples/monitor.js
@@ -1,12 +1,32 @@
 'use strict';
 
+let argv = require('yargs')
+  .usage('Monitor redis topic')
+  .default('h', 'localhost')
+  .default('p', 6379)
+  .default('c', 'pinball')
+  .alias('h', 'host')
+  .alias('p', 'port')
+  .alias('c', 'channel')
+  .describe('p', 'Port')
+  .describe('h', 'Host')
+  .describe('c', 'Channel')
+  .help('help')
+  .argv;
+
 let redis  = require('redis');
-let client = redis.createClient();
+let client = redis.createClient(argv.port, argv.host);
 let pj     = require('prettyjson');
+let tty    = require('tty');
+let json   = !tty.isatty(1);
 
 client.on('message', function(channel, message) {
-  console.log(pj.renderString(message));
-  console.log('============================================');
+  if (json) {
+    console.log(message);
+  } else {
+    console.log(pj.renderString(message));
+    console.log('============================================');
+  }
 });
 
-client.subscribe('pinball');
+client.subscribe(argv.channel);
