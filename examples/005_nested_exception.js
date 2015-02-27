e0f19f9132e3f7116348588a52d5c950d07ca82b
Alberto Miorin <github@ululi.it>
2015-02-27 09:27:18 +0100

    add examples nested exceptions

---
+++ examples/005_nested_exception.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let co         = require('co');
+let pinball    = require('..')();
+let prettyjson = require('prettyjson');
+let promise    = require('bluebird');
+require('colors');
+
+// add a transport
+pinball.use('eventemitter');
+// add a microservice
+pinball.add({ role:'first', cmd:'cmd' }, first);
+pinball.add({ role:'second', cmd:'cmd' }, second);
+pinball.add({ role:'third', cmd:'cmd' }, third);
+
+// consumer of a microservice
+co(function *() {
+  try {
+    yield pinball.act({ role:'first', cmd:'cmd' });
+  } catch(e) {
+    console.log(e);
+    console.log(e.stack);
+  }
+  pinball.close();
+});
+
+// a microservice is a generator
+function *first(act) {
+  yield act({ role:'second', cmd:'cmd' });
+}
+
+// a microservice is a generator
+function *second(act) {
+  yield act({ role:'third', cmd:'cmd' });
+}
+
+function *third() {
+  throw new CustomError('This is a custom error');
+}
+
+function CustomError(message) {
+  Error.call(this);
+  Error.captureStackTrace(this, CustomError);
+  this.message = message;
+  this.name = 'CustomError';
+}
+CustomError.prototype = Object.create(Error.prototype);
